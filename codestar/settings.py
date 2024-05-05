6ed0c49f9e2851c936f42ae7f31ecedd4f0003e6
jbillcliffe <jbillcliffe@googlemail.com>
2024-05-05 14:26:32 +0000

    add post listing to home page

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-l!$d7eaba+hx%dxp&-%wmubwra)o58#vw*00rq-6es@b^=h4qr'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-jbillcliffe-django-blog-va4v94q241.us1.codeanyapp.com',
                 '.herokuapp.com']
